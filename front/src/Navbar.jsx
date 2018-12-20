34a5290ddc2ad26952ab433713fe0572ad3934c6
Elizaveta Patrusheva <elizaveta.np@gmail.com>
2018-12-20 22:52:10 +0100

    CSS navbar et début composant Success

--- front/src/Navbar.jsx
+++ front/src/Navbar.jsx
@@ -1,30 +1,42 @@
 import React, { Component } from "react";
+import { Route, Switch, BrowserRouter, NavLink } from "react-router-dom";
 import "./styles/App.css";
 import "./navbar.css";
-
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
+import Team from "./components/Team";
+import Challenges from "./components/Challenge";
 
 class Navigation extends Component {
   render() {
     return (
-      <div>
-        <Navbar className="mynavbar">
-          <NavbarBrand className="logohome" href="/">
-            <img src="http://achat-tampon.fr/wp-content/uploads/2017/02/rond-logo.jpg" />
-          </NavbarBrand>
-          <Nav className="navigation">
-            <NavItem>
-              <NavLink href="#">Mes challenges</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">Mon équipe</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">Mes réussites</NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar className="mynavbar">
+            <NavbarBrand className="logohome" href="/">
+              <img src="http://achat-tampon.fr/wp-content/uploads/2017/02/rond-logo.jpg" />
+            </NavbarBrand>
+            <div className="navigation">
+              <NavItem>
+                <NavLink exact to="/" activeClassName="activeLien">
+                  Mes challenges
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/team" activeClassName="activeLien">
+                  Mon équipe
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/success">Mes réussites</NavLink>
+              </NavItem>
+            </div>
+          </Navbar>
+          <Switch>
+            <Route exact path="/" component={Challenges} />
+            <Route exact path="/team" component={Team} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
