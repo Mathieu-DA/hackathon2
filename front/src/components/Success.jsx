24ea77531747eff87dac03c6b91bc12950d40a01
Elizaveta Patrusheva <elizaveta.np@gmail.com>
2018-12-21 09:51:50 +0100

    Mes nouvelles réussites

--- front/src/components/Success.jsx
+++ front/src/components/Success.jsx
@@ -4,7 +4,7 @@ import "../styles/success.css";
 class Success extends Component {
   render() {
     return (
-      <div>
+      <div className="success">
         <h2>Mes badges</h2>
         <div className="badges">
           <div className="badgeItem">
@@ -36,6 +36,21 @@ class Success extends Component {
             <img src="https://i.ebayimg.com/images/g/-UIAAOxymcdRf1Ie/s-l300.jpg" />
             <p>Obtenu le 12/12/2018</p>
             <p>30 points cumulés</p>
+          </div>
+        </div>
+        <div className="reussites">
+          <h2>Mes nouvelles habitudes</h2>
+          <div className="reussiteItem">
+            <div className="circle">1</div>
+            <div className="reussiteText">Ma réussite numéro 1</div>
+          </div>
+          <div className="reussiteItem">
+            <div className="circle">2</div>
+            <div className="reussiteText">Ma réussite numéro 2</div>
+          </div>
+          <div className="reussiteItem">
+            <div className="circle">3</div>
+            <div className="reussiteText">Ma réussite numéro 3</div>
           </div>
         </div>
       </div>
